7ced89e1d55150d651cb0058804aea0a50d68c69
TumpaSharmaMitu125 <tumpasharmamitu@gmail.com>
2025-08-03 23:15:22 +0600

    Create VariableExample.java

---
+++ VariableExample.java
@@ -0,0 +1,9 @@
+public class VariablesExample {
+  public static void main(String[] args) {
+    int myNum = 25;
+    String myText = "Java";
+
+    System.out.println(myNum);
+    System.out.println(myText);
+  }
+}
