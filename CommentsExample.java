4034405e4673a459f4d79cad39fe610fa17d6d2a
TumpaSharmaMitu125 <tumpasharmamitu@gmail.com>
2025-08-03 23:16:14 +0600

    Create CommentsExample.java

---
+++ CommentsExample.java
@@ -0,0 +1,11 @@
+public class CommentsExample {
+  public static void main(String[] args) {
+    // This is a single-line comment
+    System.out.println("Comments in Java");
+
+    /*
+      This is a multi-line comment.
+      It spans across multiple lines.
+    */
+  }
+}
